d76b8461823a886b905acb313ee61eb6e9cb32b6
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:44 +0000

    Add support for mjs files (ES Modules)

---
+++ js/select.semanticui.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-select";export default DataTable;
---
+++ js/select.semanticui.mjs
@@ -0,0 +1,13 @@
+
+/*! Semanic UI styling wrapper for Select
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-se';
+import DataTable from 'datatables.net-select';
+
+
+
+
+export default DataTable;
