4b444bbe06763d06c7b45c2d1364c6a829854bb8
Allan Jardine <allan@datatables.net>
2022-10-27 15:15:24 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-Select-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Select-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Select-SemanticUI integration with SemanticUI exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
