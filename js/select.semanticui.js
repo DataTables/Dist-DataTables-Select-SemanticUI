18fdbb3c85778c9448510b616f65bde0c21f77b4
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 17:03:09 +0000

    91b668a07f24d3c8026d9ad8d2229def87eaf842 Release 1.5.0
    Sync to source repo @91b668a07f24d3c8026d9ad8d2229def87eaf842

--- js/select.semanticui.js
+++ js/select.semanticui.js
@@ -1,4 +1,3 @@
-
 /*! Semanic UI styling wrapper for Select
  * © SpryMedia Ltd - datatables.net/license
  */
--- js/select.semanticui.mjs
+++ js/select.semanticui.mjs
@@ -1,11 +1,10 @@
-
 /*! Semanic UI styling wrapper for Select
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-se';
-import DataTable from 'datatables.net-select';
+import 'datatables.net-select';
 
 
 
