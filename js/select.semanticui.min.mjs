fab5c921e65c411b6ea2bbf6feda78f66bdf8d3b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-07 15:41:19 +0100

    b49957493fd0a85464b31d4861ef8e4aa5b87435 Dev: Update active class on Buttons to `dt-button-active` for recent change in Buttons
    2c8505a79c18e7e728875d50178c5e6fbe122ba6 Update: Examples all now have VanillaJS code and are correctly formatted
    
    2a89316bd5581ab5a2677c0cbd8b52512b6db77b Merge branch 'master' of github.com:DataTables/Select
    
    Sync to source repo @2a89316bd5581ab5a2677c0cbd8b52512b6db77b

--- js/select.semanticui.min.mjs
+++ js/select.semanticui.min.mjs
@@ -1,4 +1,4 @@
 /*! Semanic UI styling wrapper for Select
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-se";import select from"datatables.net-select";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-se";import select from"datatables.net-select";let $=jQuery;export default DataTable;
