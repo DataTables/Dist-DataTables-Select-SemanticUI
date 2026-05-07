bdfca85404a17ab1eb71b3104814611ab2b74885
dtbuild <dtbuild@rigil>
2026-05-07 14:01:47 +0000

    a46f61b466102bb85f9c0ff45772e0e300d59d68
    Sync to source repo @76c586acb7863acb18de62411d11f481e0d44a0f

--- js/select.semanticui.min.mjs
+++ js/select.semanticui.min.mjs
@@ -1,4 +1,4 @@
-/*! Semanic UI styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
+/*! Select Fomantic UI styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-se";import select from"datatables.net-select";let $=jQuery;export default DataTable;
+import DataTable from"datatables.net-se";import select from"datatables.net-select";export default DataTable;
--- js/select.semanticui.mjs
+++ js/select.semanticui.mjs
@@ -1,14 +1,11 @@
-/*! Semanic UI styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
+/*! Select Fomantic UI styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-se';
 import select from 'datatables.net-select';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
